d3620c0850a3b6190203cef95e61b656acc66734
Rahul Singla <rahulsinglakumar5@gmail.com>
2025-04-11 20:36:20 -0600

    Updated Prometheus config and training runner for metrics

--- run_with_metrics.sh
+++ run_with_metrics.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Remove existing training container if any
-docker rm -f training-metrics 2>/dev/null 
+docker rm -f training-metrics 2>/dev/null
 
 echo "Starting training with metrics monitoring..."
 
-# Run new training container with fixed name
 docker run \
   --name training-metrics \
   --network=ml_project_ml-network \
